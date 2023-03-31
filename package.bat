2629a73cf7552a21e692f735c76b7433e9b851c8
alvv <alvv@demant.com>
2023-03-31 14:03:06 +0200

    removed splath screen from package

--- package.bat
+++ package.bat
@@ -3,6 +3,5 @@ pyinstaller --noconfirm ^
     --add-data .conda\lib\site-packages\customtkinter;customtkinter ^
     --add-data XYpillar.png;. ^
     --add-data XYpillar.ico;. ^
-    --splash="XYpillar.ico" ^
     --icon="XYpillar.ico" ^
     xypillar.py
